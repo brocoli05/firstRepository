b5faca0bdbf0fc63b676f44a5e0568f43ce06fe1
MJ <mjkimthebest@gmail.com>
2022-08-26 10:29:41 +0900

    ignore test

---
+++ src/jquerydemo/User.java
@@ -0,0 +1,17 @@
+package jquerydemo;
+
+public class User {
+	String name;
+	int age;
+	String address;
+	
+	public User(String name, int age, String address) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "이름: "+name+"나이:"+age+"주소:"+address+"TEST";
+	}
+}
